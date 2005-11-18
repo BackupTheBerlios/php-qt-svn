42a88cda425121fd66437ec56de74a5ae5d391ef
tm243 <tm243@52be3f7a-a203-0410-862e-b73c8613a978>
2005-11-18 19:31:12 +0000

    new test: instances.

---
+++ trunk/tests/instances.php
@@ -0,0 +1,17 @@
+<?
+  if(!extension_loaded('php_qt')) {
+    dl('php_qt.' . PHP_SHLIB_SUFFIX);
+  }
+ 
+  $app = new QCoreApplication();
+
+  echo $app." app\n";  
+
+  $instance_static = QCoreApplication::instance();
+  $instance = $app->instance(); 
+ 
+  echo $instance_static." QCoreApplication::instance()\n";
+  echo $instance." app->instance()\n";
+  echo $app." app\n";  
+
+?>
