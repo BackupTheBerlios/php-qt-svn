735adf1e64fa528493676708811ae9eeadf77271
tm243 <tm243@52be3f7a-a203-0410-862e-b73c8613a978>
2005-11-18 17:32:04 +0000

    new tutorial: t4

---
+++ trunk/tutorials/t4/main.php
@@ -0,0 +1,44 @@
+<?php
+
+    /****************************************************************
+    **
+    ** Qt tutorial 4
+    **
+    ****************************************************************/
+                
+	if(!extension_loaded('php_qt')) {
+		dl('php_qt.' . PHP_SHLIB_SUFFIX);
+	}
+
+    class MyWidget extends QWidget
+    {
+    
+        public $quit;
+
+        function __construct()
+        {
+
+			parent::__construct();
+
+//            setFixedSize(200, 120);
+
+            $this->quit = new QPushButton("Quit", $this);
+            $this->quit->setGeometry(62, 40, 75, 30);
+//            $quit->setFont();
+
+            /* the macro qApp is not available in php-qt, we use QCoreApplication::instance() directly. */
+            $this->connect($this->quit, SIGNAL("clicked()"), QCoreApplication::instance(), SLOT("quit()"));
+
+        }
+    
+    }
+
+    $app = new QApplication();
+
+    $widget = new MyWidget();
+
+    $widget->show();
+
+    $app->exec();
+
+?>
