dd50e271993709703ddb9bd802ba5a95f809aa27
tm243 <tm243@52be3f7a-a203-0410-862e-b73c8613a978>
2005-12-11 14:17:33 +0000

    tutorial 5 added.

---
+++ trunk/tutorials/t5/main.php
@@ -0,0 +1,54 @@
+<?php
+    /****************************************************************
+    **
+    ** Qt tutorial 5
+    **
+    ****************************************************************/
+
+	if(!extension_loaded('php_qt')) {
+		dl('php_qt.' . PHP_SHLIB_SUFFIX);
+	}
+
+    class MyWidget extends QWidget
+    {
+
+        public $quit;
+        public $slider;
+        public $layout;
+        public $lcd;
+
+        function __construct(){
+
+			parent::__construct();
+
+            $this->quit = new QPushButton("Quit", $this);
+//            $this->quit->setFont(QFont("Times", 18, QFont::Bold));
+
+            $this->lcd = new QLCDNumber(2,$this);
+
+            $this->slider = new QSlider($this);
+            $this->slider->setOrientation(QT_ORIENTATIONS_HORIZONTAL);
+            $this->slider->setRange(0, 99);
+            $this->slider->setValue(0);
+
+            $this->connect($this->quit, SIGNAL("clicked()"), QApplication::instance(), SLOT("quit()"));
+            $this->connect($this->slider, SIGNAL("valueChanged(int)"),$this->lcd, SLOT("display(int)"));
+
+            $this->layout = new QVBoxLayout($this);
+
+            $this->layout->addWidget($this->quit);
+            $this->layout->addWidget($this->lcd);
+            $this->layout->addWidget($this->slider);
+            $this->setLayout($this->layout);
+
+        }
+    }
+
+
+    $app = new QApplication();
+    $widget = new MyWidget();
+    $widget->show();
+
+    $app->exec();
+
+?>
