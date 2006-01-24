23a49dbb32220c6ffa9176e024f99e47e9881688
tm243 <tm243@52be3f7a-a203-0410-862e-b73c8613a978>
2006-01-24 10:07:51 +0000

    typos fixed.

--- trunk/tests/qstring.php
+++ trunk/tests/qstring.php
@@ -11,34 +11,34 @@
 	$QString_2 = new QString(40);
 	$QString_3 = new QString(41);
 
-/* this works: */
+/* this work: */
 	echo $QString_2;
 	echo $QString_1;
 	echo $QString_3;
     echo "\n";
 
-/* this does not works */
+/* this does not work */
 	echo $QString_2." ".$QString_1." ".$QString_3;
     echo "\n";
 
-/* this does not works */
+/* this does not work */
 	$tmp = $QString_2." ".$QString_1." ".$QString_3;
 	print($tmp);
 
     echo "\n";
 
-/* this does not works */
+/* this does not work */
 	$tmp2 = $QString_2;
 	$tmp2 = $QString_1;
 	$tmp2 = $QString_3;
 	echo $tmp;
     echo "\n";
 
-/* this does not works */
+/* this does not work */
 	echo "$QString_2 $QString_1 $QString_3";
     echo "\n";
 
-/* this works: */
+/* this work: */
 	$var1 = $QString_1->__toString();
 	$var2 = $QString_2->__toString();
 	$var3 = $QString_3->__toString();
