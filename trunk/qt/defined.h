d0d077bfb8bd54310df25a57377588737dff1891
tm243 <tm243@52be3f7a-a203-0410-862e-b73c8613a978>
2006-06-06 20:27:57 +0000

    cpp defines of implemented classes for safety compilation.

---
+++ trunk/qt/defined.h
@@ -0,0 +1,292 @@
+#ifndef PHP_QT_QAbstractButton
+#define PHP_QT_QAbstractButton
+#endif
+
+#ifndef PHP_QT_QAbstractSlider
+#define PHP_QT_QAbstractSlider
+#endif
+
+#ifndef PHP_QT_QActionEvent
+#define PHP_QT_QActionEvent
+#endif
+
+#ifndef PHP_QT_QApplication
+#define PHP_QT_QApplication
+#endif
+
+#ifndef PHP_QT_QBoxLayout
+#define PHP_QT_QBoxLayout
+#endif
+
+#ifndef PHP_QT_QChar
+#define PHP_QT_QChar
+#endif
+
+#ifndef PHP_QT_QCharRef
+#define PHP_QT_QCharRef
+#endif
+
+#ifndef PHP_QT_QChildEvent
+#define PHP_QT_QChildEvent
+#endif
+
+#ifndef PHP_QT_QClipboardEvent
+#define PHP_QT_QClipboardEvent
+#endif
+
+#ifndef PHP_QT_QCloseEvent
+#define PHP_QT_QCloseEvent
+#endif
+
+#ifndef PHP_QT_QContextMenuEvent
+#define PHP_QT_QContextMenuEvent
+#endif
+
+#ifndef PHP_QT_QCoreApplication
+#define PHP_QT_QCoreApplication
+#endif
+
+#ifndef PHP_QT_QDate
+#define PHP_QT_QDate
+#endif
+
+#ifndef PHP_QT_QDateTime
+#define PHP_QT_QDateTime
+#endif
+
+#ifndef PHP_QT_QDragEnterEvent
+#define PHP_QT_QDragEnterEvent
+#endif
+
+#ifndef PHP_QT_QDragLeaveEvent
+#define PHP_QT_QDragLeaveEvent
+#endif
+
+#ifndef PHP_QT_QDragMoveEvent
+#define PHP_QT_QDragMoveEvent
+#endif
+
+#ifndef PHP_QT_QDragResponseEvent
+#define PHP_QT_QDragResponseEvent
+#endif
+
+#ifndef PHP_QT_QDropEvent
+#define PHP_QT_QDropEvent
+#endif
+
+#ifndef PHP_QT_QEvent
+#define PHP_QT_QEvent
+#endif
+
+#ifndef PHP_QT_QFileOpenEvent
+#define PHP_QT_QFileOpenEvent
+#endif
+
+#ifndef PHP_QT_QFocusEvent
+#define PHP_QT_QFocusEvent
+#endif
+
+#ifndef PHP_QT_QFont
+#define PHP_QT_QFont
+#endif
+
+#ifndef PHP_QT_QFrame
+#define PHP_QT_QFrame
+#endif
+
+#ifndef PHP_QT_QGridLayout
+#define PHP_QT_QGridLayout
+#endif
+
+#ifndef PHP_QT_QHBoxLayout
+#define PHP_QT_QHBoxLayout
+#endif
+
+#ifndef PHP_QT_QHelpEvent
+#define PHP_QT_QHelpEvent
+#endif
+
+#ifndef PHP_QT_QHideEvent
+#define PHP_QT_QHideEvent
+#endif
+
+#ifndef PHP_QT_QHoverEvent
+#define PHP_QT_QHoverEvent
+#endif
+
+#ifndef PHP_QT_QIconDragEvent
+#define PHP_QT_QIconDragEvent
+#endif
+
+#ifndef PHP_QT_QInputEvent
+#define PHP_QT_QInputEvent
+#endif
+
+#ifndef PHP_QT_QInputMethodEvent
+#define PHP_QT_QInputMethodEvent
+#endif
+
+#ifndef PHP_QT_Attribute
+#define PHP_QT_Attribute
+#endif
+
+#ifndef PHP_QT_QInternal
+#define PHP_QT_QInternal
+#endif
+
+#ifndef PHP_QT_QKeyEvent
+#define PHP_QT_QKeyEvent
+#endif
+
+#ifndef PHP_QT_QLCDNumber
+#define PHP_QT_QLCDNumber
+#endif
+
+#ifndef PHP_QT_QLabel
+#define PHP_QT_QLabel
+#endif
+
+#ifndef PHP_QT_QLatin1Char
+#define PHP_QT_QLatin1Char
+#endif
+
+#ifndef PHP_QT_QLatin1String
+#define PHP_QT_QLatin1String
+#endif
+
+#ifndef PHP_QT_QLayout
+#define PHP_QT_QLayout
+#endif
+
+#ifndef PHP_QT_QLayoutItem
+#define PHP_QT_QLayoutItem
+#endif
+
+#ifndef PHP_QT_QLineEdit
+#define PHP_QT_QLineEdit
+#endif
+
+#ifndef PHP_QT_QMouseEvent
+#define PHP_QT_QMouseEvent
+#endif
+
+#ifndef PHP_QT_QMoveEvent
+#define PHP_QT_QMoveEvent
+#endif
+
+#ifndef PHP_QT_QObject
+#define PHP_QT_QObject
+#endif
+
+#ifndef PHP_QT_QObjectData
+#define PHP_QT_QObjectData
+#endif
+
+#ifndef PHP_QT_QObjectUserData
+#define PHP_QT_QObjectUserData
+#endif
+
+#ifndef PHP_QT_QPaintDevice
+#define PHP_QT_QPaintDevice
+#endif
+
+#ifndef PHP_QT_QPaintEvent
+#define PHP_QT_QPaintEvent
+#endif
+
+#ifndef PHP_QT_QPainter
+#define PHP_QT_QPainter
+#endif
+
+#ifndef PHP_QT_QPushButton
+#define PHP_QT_QPushButton
+#endif
+
+#ifndef PHP_QT_QResizeEvent
+#define PHP_QT_QResizeEvent
+#endif
+
+#ifndef PHP_QT_QShortcutEvent
+#define PHP_QT_QShortcutEvent
+#endif
+
+#ifndef PHP_QT_QShowEvent
+#define PHP_QT_QShowEvent
+#endif
+
+#ifndef PHP_QT_QSlider
+#define PHP_QT_QSlider
+#endif
+
+#ifndef PHP_QT_QSpacerItem
+#define PHP_QT_QSpacerItem
+#endif
+
+#ifndef PHP_QT_QStatusTipEvent
+#define PHP_QT_QStatusTipEvent
+#endif
+
+#ifndef PHP_QT_QString
+#define PHP_QT_QString
+#endif
+
+#ifndef PHP_QT_Data
+#define PHP_QT_Data
+#endif
+
+#ifndef PHP_QT_Null
+#define PHP_QT_Null
+#endif
+
+#ifndef PHP_QT_QTabletEvent
+#define PHP_QT_QTabletEvent
+#endif
+
+#ifndef PHP_QT_QTime
+#define PHP_QT_QTime
+#endif
+
+#ifndef PHP_QT_QTimer
+#define PHP_QT_QTimer
+#endif
+
+#ifndef PHP_QT_QTimerEvent
+#define PHP_QT_QTimerEvent
+#endif
+
+#ifndef PHP_QT_QToolBarChangeEvent
+#define PHP_QT_QToolBarChangeEvent
+#endif
+
+#ifndef PHP_QT_QVBoxLayout
+#define PHP_QT_QVBoxLayout
+#endif
+
+#ifndef PHP_QT_QWhatsThisClickedEvent
+#define PHP_QT_QWhatsThisClickedEvent
+#endif
+
+#ifndef PHP_QT_QWheelEvent
+#define PHP_QT_QWheelEvent
+#endif
+
+#ifndef PHP_QT_QWidget
+#define PHP_QT_QWidget
+#endif
+
+#ifndef PHP_QT_QWidgetData
+#define PHP_QT_QWidgetData
+#endif
+
+#ifndef PHP_QT_QWidgetItem
+#define PHP_QT_QWidgetItem
+#endif
+
+#ifndef PHP_QT_QWindowStateChangeEvent
+#define PHP_QT_QWindowStateChangeEvent
+#endif
+
+#ifndef PHP_QT_Qt
+#define PHP_QT_Qt
+#endif
+
